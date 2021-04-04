06fc35f3a14d77b39189cf4df6317d92e123dcdf
DoniZhelev <donizhelev@gmail.com>
2021-04-04 22:18:54 +0300

    add Landarmk card component

---
+++ spa/src/components/LandmarkCard/LandmarkCard.js
@@ -0,0 +1,21 @@
+import { Link} from 'react-router-dom';
+
+const LandmarkCard = () => {
+
+
+    return();
+}
+
+<div className="row">
+<div className="col-6 col-12-narrower">
+
+    <section>
+        <Link to="" className="image featured"><img src="https://www.nasamnatam.com/statiifiles/Klearchos%20Kapoutsis_Belogradchik.jpg" alt="" /></Link>
+        <header>
+            <h3>Belogradchik Rocks</h3>
+        </header>
+        <p>The formation of the Belogradchik Rocks started during the Permian period, about 230 million years ago, when the tectonic cycle was folding to create the Balkan Mountains and elevating the region of Belogradchik. </p>
+    </section>
+
+</div>
+</div>
